a665a667a0dc737940b3470714a028d3477370c8
gokay <mail@gokay.de>
2022-12-03 13:43:48 +0100

    finetune rate limiter

--- handler/limiter/limiter.go
+++ handler/limiter/limiter.go
@@ -1,48 +1,73 @@
 package limiter
 
 import (
+	"strings"
 	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
 
 type IPRateLimiter struct {
-	ips map[string]*rate.Limiter
-	mu  *sync.RWMutex
-	r   rate.Limit
-	b   int
+	ips  map[string]*rate.Limiter
+	seen map[string]time.Time
+	mu   *sync.RWMutex
+	r    rate.Limit
+	b    int
 }
 
 func NewIPRateLimiter(r rate.Limit, b int) *IPRateLimiter {
+
 	i := &IPRateLimiter{
-		ips: make(map[string]*rate.Limiter),
-		mu:  &sync.RWMutex{},
-		r:   r,
-		b:   b,
+		ips:  make(map[string]*rate.Limiter),
+		seen: make(map[string]time.Time),
+		mu:   &sync.RWMutex{},
+		r:    r,
+		b:    b,
 	}
 
 	return i
 }
 
-func (i *IPRateLimiter) AddIP(ip string) *rate.Limiter {
-	i.mu.Lock()
-	defer i.mu.Unlock()
-
-	limiter := rate.NewLimiter(i.r, i.b)
-
-	i.ips[ip] = limiter
-
-	return limiter
-}
-
 func (i *IPRateLimiter) GetLimiter(ip string) *rate.Limiter {
+
+	if offset := strings.LastIndex(ip, ":"); offset > 7 {
+		ip = ip[:offset]
+	}
+
 	i.mu.Lock()
+
 	limiter, exists := i.ips[ip]
 
 	if !exists {
-		i.mu.Unlock()
-		return i.AddIP(ip)
+
+		// housekeeping
+
+		if len(i.ips) > 100 {
+
+			var orphan []string
+
+			for k, v := range i.seen {
+				if time.Since(v) > 15*time.Minute {
+					orphan = append(orphan, k)
+				}
+			}
+
+			for k := range orphan {
+				delete(i.ips, orphan[k])
+				delete(i.seen, orphan[k])
+			}
+
+		}
+
+		// new limiter
+
+		limiter = rate.NewLimiter(i.r, i.b)
+		i.ips[ip] = limiter
+
 	}
+
+	i.seen[ip] = time.Now()
 
 	i.mu.Unlock()
 
